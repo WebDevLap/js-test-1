398b214cc5febb0827318b74c048ad7024ac1bee
Akhmed <afandi2020.r@mail.ru>
2023-11-18 13:03:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.write(
+  navigator.vibrate(0) ? '<div class="vibrate">your phone is vibrate</div>' : 'NOT VIBRATE',
+);
---
+++ script.ts
@@ -0,0 +1,8 @@
+let t = {
+  r: 1,
+  t: 1,
+};
+
+for (let el in t) {
+  console.log(el);
+}
