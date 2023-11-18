aa7a9e49e5796ec4bce19a84406513a677791bc1
Akhmed <afandi2020.r@mail.ru>
2023-11-18 13:30:53 +0300

    sec

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.write(
   navigator.vibrate(0) ? '<div class="vibrate">your phone is vibrate</div>' : 'NOT VIBRATE',
 );
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+console.log(window.navigator.vibrate(100) ? 'vibrate' : 'not vibrate')
+window.navigator.vibrate(5000);
